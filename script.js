94442410a4beda8d779a9430fdd789b9a6169c58
emilyamccarthy <46628784+emilyamccarthy@users.noreply.github.com>
2022-05-06 11:49:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,177 +1,48 @@
-var map = L.map('mapid').setView([38.63868,-90.30317], 12);
+require([
+      "esri/views/MapView",
+      "esri/WebMap",
+      "esri/widgets/Legend",
+      "dojo/domReady!"
+    ], function(
+      MapView, WebMap, Legend
+    ) {
 
-  // load a tile layer
- L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-function onEachFeature(feature, layer) {
-  if (feature.properties && feature.properties.popupContent) {
-    layer.bindPopup(feature.properties.popupContent);
-  }
-}
-
-var geojsonFeature = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "Name": "Sauce on the Side",
-        "popupContent": "<b>Sauce on the Side</b><br>Great calzones and salads"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.2565586566925,
-          38.627172226771336
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Name": "Seoul  Taco",
-        "popupContent": "<b>Seoul Taco</b><br>Korean-mexican fusion, I love the burritos"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.24987995624542,
-          38.628341454584714
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Name": "U-City Grill",
-        "popupContent": "<b>U-City Grill</b><br>Cash only; delicicous beef bulgogi"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.30848622322081,
-          38.65663598042729
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Name": "801 Chophouse",
-        "popupContent": "<b>801 Chophouse</b><br>Elegant steakhouse"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.33247590065001,
-          38.64892763595949
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "Name": "Charlie Gitto's",
-        "popupContent": "<b>Charlie Gitto's</b><br>Best creme brulee in STL"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -90.27347803115843,
-          38.61791831050015
-        ]
-      }
-    }
-  ]
-};
-
-L.geoJSON(geojsonFeature, {
-  onEachFeature: onEachFeature
-}).addTo(map);
-
-//var feat = L.geoJSON(geojsonFeature).addTo(map);
-
-//feat.bindPopup("help").openPopup();
-
-var myLines = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "LineString",
-        "coordinates": [
-          [
-            -106.5234375,
-            35.10193405724606
-          ],
-          [
-            -106.41357421875,
-            35.37113502280101
-          ],
-          [
-            -106.0400390625,
-            35.567980458012094
-          ],
-          [
-            -105.79833984375,
-            35.69299463209881
-          ],
-          [
-            -105.6005859375,
-            35.47856499535729
-          ],
-          [
-            -105.40283203124999,
-            35.460669951495305
-          ],
-          [
-            -105.09521484375,
-            35.69299463209881
-          ],
-          [
-            -104.83154296875,
-            35.94243575255426
-          ],
-          [
-            -104.74365234375,
-            36.049098959065645
-          ],
-          [
-            -104.6337890625,
-            36.2265501474709
-          ],
-          [
-            -104.6337890625,
-            36.421282443649496
-          ]
-        ]
-      }
-    }
-  ]
-}
-
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 2,
-    "opacity": 0.65
-};
-
-L.geoJSON(myLines, {
-    style: myStyle
-}).addTo(map);
-
-
-
-L.geoJSON(states, {
-    style: function(feature) {
-        switch (feature.properties.party) {
-            case 'Republican': return {color: "#C8C9C7"};
-            case 'Democrat':   return {color: "#003DA5"};
+      /************************************************************
+       * Creates a new WebMap instance. A WebMap must reference
+       * a PortalItem ID that represents a WebMap saved to
+       * arcgis.com or an on-premise portal.
+       *
+       * To load a WebMap from an on-premise portal, set the portal
+       * url with esriConfig.portalUrl.
+       ************************************************************/
+      var webmap = new WebMap({
+        portalItem: { // autocasts as new PortalItem()
+          id: "b70b3ab2603442819012f123aa87b4df"
         }
-    }
-}).addTo(map);
+      });
+      /************************************************************
+       * Set the WebMap instance to the map property in a MapView.
+       ************************************************************/
+      var view = new MapView({
+        map: webmap,
+        center: [-90.22759080968054, 38.6], //Longitude, latitude
+        zoom: 4.5,
+        container: "viewDiv"
+      });
+  
+   view.when(() => {
+     const featureLayer = webmap.layers.getItemAt(0);
+     
+     const legend = new Legend({
+       view:view,
+       layerInfos: [
+         {
+           layer: featureLayer, 
+           title: "Percent Change in Total Employment from February 2020"
+         }
+       ]
+     }); 
+    view.ui.add(legend, "bottom-right");
+   });
+  });
+  
